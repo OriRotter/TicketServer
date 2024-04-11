a621e13386e55304aa18acfc5f3478f369569b42
Ori Rotter <80815454+OriRotter@users.noreply.github.com>
2024-04-11 13:22:38 +0300

    changed 3.0

---
+++ database_control.py
@@ -0,0 +1,151 @@
+import sqlite3
+from Strings import *
+
+
+def use_hash(show_id, ticket_hash):
+    db_path = hash_path(showID=show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        used = cursor.execute('SELECT Used FROM Tickets WHERE Hash = ?', (ticket_hash,)).fetchone()
+
+        if used is None:
+            return "404 no such hash."
+
+        if not used[0]:
+            cursor.execute('UPDATE Tickets SET Used = 1 WHERE Hash = ?', (ticket_hash,))
+            conn.commit()
+            return "200 OK"
+
+        return "300 Hash used."
+
+
+def check_seat(show_id, seat):
+    db_path = hash_path(showID=show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        check = cursor.execute('SELECT 1 FROM Tickets WHERE Row = ? AND Column = ? AND Place = ?',
+                               (seat.row, seat.column, seat.place)).fetchone()
+
+        return check is None
+
+
+def get_info_by_hash(show_id, ticket_hash):
+    db_path = hash_path(showID=show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        ticket_info = cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
+                                        FROM Tickets WHERE Hash = ?''', (ticket_hash,)).fetchone()
+
+        return ticket_info or "404 no such hash."
+
+
+def get_info_by_something(show_id, something):
+    db_path = hash_path(showID=show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    ticket_info = []
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        try:
+            ticket_info += cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
+                                            FROM Tickets WHERE OrderNumber = ? OR TicketNumber = ? OR Hash = ? ''',
+                                          (something, something, something)).fetchone()
+        except sqlite3.Error:
+            pass
+
+    order_db_path = order_path(showID=show_id)
+    if not os.path.exists(order_db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(order_db_path) as conn:
+        cursor = conn.cursor()
+        try:
+            ticket_info += cursor.execute('''SELECT OrderNumber, Name, Email, PhoneNumber
+                                            FROM Orders WHERE OrderNumber = ? OR Name = ? OR Email = ? OR PhoneNumber = ? ''',
+                                          (something, something, something, something)).fetchone()
+        except sqlite3.Error:
+            pass
+
+    return ticket_info or f"404 no such {something}."
+
+
+def get_info_by_ticket_number(show_id, ticket_num):
+    db_path = hash_path(showID=show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        ticket_info = cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
+                                        FROM Tickets WHERE TicketNumber = ?''', (ticket_num,)).fetchone()
+
+        return ticket_info or "404 no such ticket number."
+
+
+def store_ticket(ticket):
+    db_path = hash_path(showID=ticket.show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Tickets (OrderNumber, Place, Row, Column, TicketNumber, Used, Hash)
+                          VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                       (ticket.order_number, ticket.place, ticket.row, ticket.column,
+                        ticket.ticket_num, ticket.used, ticket.hash))
+        conn.commit()
+
+
+def get_ticket_number(ticketGroup):
+    db_path = hash_path(showID=ticketGroup.show_id)
+    if not os.path.exists(db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        max_ticket_number = cursor.execute("SELECT MAX(TicketNumber) FROM Tickets").fetchone()[0]
+        return (max_ticket_number or 0) + 1
+
+
+def get_order_number(ticketGroup):
+    order_db_path = order_path(showID=ticketGroup.show_id)
+    if not os.path.exists(order_db_path):
+        raise ValueError("No such show ID.")
+
+    with sqlite3.connect(order_db_path) as conn:
+        cursor = conn.cursor()
+        max_order_number = cursor.execute("SELECT MAX(OrderNumber) FROM Orders").fetchone()[0]
+        return (max_order_number or 0) + 1
+
+
+def store_order(ticketGroup):
+    order_db_path = order_path(showID=ticketGroup.show_id)
+    with sqlite3.connect(order_db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Orders (OrderNumber, Name, Email, PhoneNumber)
+                                      VALUES (?, ?, ?, ?)''',
+                       (ticketGroup.order_number, ticketGroup.name, ticketGroup.email, ticketGroup.phone_number))
+        conn.commit()
+
+
+def store_hashes(ticketGroup):
+    for ticket in ticketGroup.tickets:
+        store_ticket(ticket)
+
+
+def get_tickets(ticketGroup):
+    ticket_info = {}
+    for ticket in ticketGroup.tickets:
+        ticket_info[f"{ticket.hash},{ticket.show_id}"] = [
+            ticket.order_number, ticket.place, ticket.row, ticket.column, ticket.ticket_num, bool(ticket._used)]
+    return ticket_info
--- util.py
+++ util.py
@@ -1,135 +1,44 @@
-from Classes import *
-from Strings import *
-
-
-def use_hash(show_id, ticket_hash):
-    if not os.path.exists(hash_path(showID=show_id)):
-        raise ValueError("No such show ID.")
-
-    with sqlite3.connect(hash_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        used = cursor.execute('SELECT 1 FROM Tickets WHERE Row = 2 AND Column = 2', (ticket_hash,)).fetchone()
-
-        if used is None:
-            return "404 no such hash."
-
-        if not used[0]:
-            cursor.execute('UPDATE Tickets SET Used = 1 WHERE Hash = ?', (ticket_hash,))
-            conn.commit()
-            return "200 OK"
-
-        return "300 Hash used."
-
-
-def check_seat(show_id, seat):
-    if not os.path.exists(hash_path(showID=show_id)):
-        raise ValueError("No such show ID.")
-
-    with sqlite3.connect(hash_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        check = cursor.execute('SELECT 1 FROM Tickets WHERE Row = ? AND Column = ? AND Place = ?',
-                               (seat.row, seat.column, seat.place)).fetchone()
-
-        if check is None:
-            return True
-        return False
-
-
-def get_info_by_hash(show_id, ticket_hash):
-    if not os.path.exists(hash_path(showID=show_id)):
-        raise ValueError("No such show ID.")
-
-    with sqlite3.connect(hash_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        ticket_info = cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
-                                        FROM Tickets WHERE Hash = ?''', (ticket_hash,)).fetchone()
-
-        if ticket_info is None:
-            return "404 no such hash."
-
-        return ticket_info
-
-
-def get_info_by_somthing(show_id, something):
-    if not os.path.exists(hash_path(showID=show_id)):
-        raise ValueError("No such show ID.")
-
-    ticket_info = []
-    with sqlite3.connect(hash_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        try:
-            ticket_info += cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
-                                            FROM Tickets WHERE OrderNumber = ? OR TicketNumber = ? OR Hash = ? ''', (something, something, something)).fetchone()
-        except:
-            pass
-
-
-
-    with sqlite3.connect(order_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        try:
-            ticket_info += cursor.execute('''SELECT OrderNumber, Name, Email, PhoneNumber
-                                            FROM Orders WHERE OrderNumber = ? OR Name = ? OR Email = ? OR PhoneNumber = ? ''', (something, something, something, something)).fetchone()
-        except:
-            pass
-    if ticket_info is None:
-        return f"404 no such {something}."
-
-    return ticket_info
-
-
-def get_info_by_ticket_number(show_id, ticket_num):
-    if not os.path.exists(hash_path(showID=show_id)):
-        raise ValueError("No such show ID.")
-
-    with sqlite3.connect(hash_path(showID=show_id)) as conn:
-        cursor = conn.cursor()
-        ticket_info = cursor.execute('''SELECT OrderNumber, Place, Row, Column, TicketNumber, Used
-                                        FROM Tickets WHERE TicketNumber = ?''', (ticket_num,)).fetchone()
-
-        if ticket_info is None:
-            return "404 no such ticket number."
-
-        return ticket_info
+from TicketGroup import *
+from database_control import check_seat, folder_path, order_path, hash_path
 
 
 def create_tickets(show_id, seats, phone_number, email, name=None):
+    # Check if seats are available
     for seat in seats:
         if not check_seat(show_id=show_id, seat=seat):
             raise ValueError(f"Seat is taken. {seat.place}:{seat.row},{seat.column}")
-    tickets = TicketGroup(show_id=show_id, seats=seats, phone_number=phone_number, email=email, name=name)
-    return tickets.get_tickets()
 
+    # Create TicketGroup
+    tickets = TicketGroup(show_id=show_id, seats=seats, phone_number=phone_number, email=email, name=name)
+    return get_tickets(tickets)
 
 
 def create_show(show_id):
+    # Create directory for the show if it doesn't exist
     try:
-        os.mkdir(folder_path(showID=show_id))
-    except FileExistsError:
-        pass
+        os.makedirs(folder_path(showID=show_id), exist_ok=True)
+    except OSError as e:
+        print(f"Error creating directory for show {show_id}: {e}")
 
-    with open(order_path(showID=show_id), 'w') as f:
-        with sqlite3.connect(order_path(showID=show_id)) as conn:
-            cursor = conn.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Orders (
-                            "OrderNumber" INTEGER NOT NULL,
-                            "Name" TEXT,
-                            "Email" TEXT NOT NULL,
-                            "PhoneNumber" TEXT NOT NULL
-                              )''')
+    # Create or connect to Orders database
+    with sqlite3.connect(order_path(showID=show_id)) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Orders (
+                        "OrderNumber" INTEGER PRIMARY KEY,
+                        "Name" TEXT,
+                        "Email" TEXT NOT NULL,
+                        "PhoneNumber" TEXT NOT NULL
+                          )''')
 
-    with open(hash_path(showID=show_id), 'w') as f:
-        with sqlite3.connect(hash_path(showID=show_id)) as conn:
-            cursor = conn.cursor()
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Tickets (
-                            "OrderNumber" INTEGER NOT NULL,
-                            "Place" TEXT,
-                            "Row" INTEGER,
-                            "Column" INTEGER,
-                            "TicketNumber" INTEGER NOT NULL,
-                            "Used" BOOLEAN NOT NULL,
-                            "Hash" TEXT NOT NULL
-                              )''')
-
-
-print(get_info_by_somthing(2,"אורי רוטר"))
+    # Create or connect to Tickets database
+    with sqlite3.connect(hash_path(showID=show_id)) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Tickets (
+                        "OrderNumber" INTEGER,
+                        "Place" TEXT,
+                        "Row" INTEGER,
+                        "Column" INTEGER,
+                        "TicketNumber" INTEGER NOT NULL PRIMARY KEY,
+                        "Used" BOOLEAN NOT NULL,
+                        "Hash" TEXT NOT NULL
+                          )''')
